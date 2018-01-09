90ab7b98e170c168a13d0aa7a52267ecde3f58e4
Osinko <yoshinari_m@msn.com>
2018-01-09 17:32:15 +0900

    no message

--- Math2017/Assets/FnFn.cs
+++ Math2017/Assets/FnFn.cs
@@ -4,21 +4,23 @@ using System.Collections.Generic;
 
 public class FnFn : MonoBehaviour
 {
-
+    //フィボナッチ数列の近似を黄金比で出力
     void Start()
     {
-        IEnumerable<float> number = GeneratorNum(5, 20);
-        foreach (var item in number)
+        int length = 10;
+        int number = 1;
+        List<int> an = new List<int>();
+
+        for (int i = 0; i < length; i++)
+        {
+            an.Add(number);
+            number = Mathf.RoundToInt(number * 1.618f);
+            //ここで重要なのは四捨五入された整数に対して黄金比が適用されている点（実数に対して黄金比を掛けるとズレる）
+        }
+
+        foreach (var item in an)
         {
             print(item);
-        }
-    }
-
-    IEnumerable<float> GeneratorNum(int x, int y)
-    {
-        for (int i = x; i <= y; i++)
-        {
-            yield return i;
         }
     }
 }
